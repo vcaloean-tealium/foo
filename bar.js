3dea09ed31af88147d58a3dbd65421cdef7d142a
Valentin Caloean <valentin.caloean@tealium.com>
2026-09-22 13:53:11 -0400

    Add html_entity_test_1 assignment in bar.js

--- bar.js
+++ bar.js
@@ -1,5 +1,7 @@
 var testChars = "\" & < > '";
 
 if (b["ut.event"] === "view" && true) {
-    b["html_entity_test"] = testChars;
+    b["html_entity_test_1"] = testChars;
 }
+
+// foo bar baz hello world, & < > "
